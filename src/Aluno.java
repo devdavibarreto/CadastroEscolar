7710c090cdae9f90cbaba05cc1f952056630c766
DaviBarretoo <davibarretojkda@gmail.com>
2025-02-21 10:16:17 -0300

    Melhorando o código

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,31 +1,45 @@
 import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Aluno {
+Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-    String nome = "";
+    String nome ="";
     String sobrenome = "";
     String cpf = "";
     String dataDeNascimento = "";
     String matriculaAtiva = "";
-    private String[] nomes;
 
-    protected void exibirDadosDoAluno(){
-        String dadosDoAluno[] = {nome,sobrenome,cpf,dataDeNascimento,matriculaAtiva};
-        System.out.println(Arrays.toString(dadosDoAluno));
 
-        for(int x = 0; x < dadosDoAluno.length;x++){
-            if (x == 0 || x == 1 || x == 2 ||x == 3){
-               continue;
+    protected String[] cadastraraluno(){
+        System.out.println("Qual o nome do aluno ? ");
+        nome = scanner.next();
+        System.out.println("Qual o Sobrenome do aluno ? ");
+    sobrenome = scanner.next();
+        System.out.println("Qual e o  cpf do aluno ? ");
+        cpf = scanner.next();
+        System.out.println("Qual e a data de nascimento do aluno ? ");
+        dataDeNascimento = scanner.next();
+        System.out.println("A matrícula do aluno esta ativa ");
+        matriculaAtiva = scanner.next();
+
+    String[] dados = {nome,sobrenome,cpf,dataDeNascimento,matriculaAtiva};
+
+    return dados;
+}
+    protected void exibirDadosDoAluno() {
+       String[] dadosDoEstudante = {nome, sobrenome, cpf, dataDeNascimento, matriculaAtiva};
+
+        System.out.println(Arrays.toString(dadosDoEstudante));
+
+        for (int x = 0; x < dadosDoEstudante.length; x++) {
+            if (x == 0 || x == 1 || x == 2 || x == 3) {
+                continue;
             }
-            System.out.println("Matrícula do aluno se encontra : " + dadosDoAluno[x]);
+            System.out.println("Matrícula do aluno se encontra Ativa : " + dadosDoEstudante[x]);
         }
-  }
+    }
 
-
-  public String ExibirAlunos(){
-
-      System.out.println("oi");
-      return "";
-  }
 
 }
